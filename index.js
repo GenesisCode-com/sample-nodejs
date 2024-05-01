6dd14eaaa110f4b190accecc9c6635467c865919
GenesisCode-com <102680020+GenesisCode-com@users.noreply.github.com>
2024-05-01 14:25:18 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  // Xử lý yêu cầu từ trình duyệt
-  if (req.url === '/trk') {
-    // Chuyển hướng người dùng sang trang web của Google
-    res.writeHead(302, { 'Location': 'https://www.google.com' });
-    res.end();
-  } else {
-    // Trả về trang không tìm thấy nếu đường dẫn không phù hợp
-    res.writeHead(404);
-    res.end();
-  }
-});
+// const server = http.createServer((req, res) => {
+//   // Xử lý yêu cầu từ trình duyệt
+//   if (req.url === '/trk') {
+//     // Chuyển hướng người dùng sang trang web của Google
+//     res.writeHead(302, { 'Location': 'https://www.google.com' });
+//     res.end();
+//   } else {
+//     // Trả về trang không tìm thấy nếu đường dẫn không phù hợp
+//     res.writeHead(404);
+//     res.end();
+//   }
+// });
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(port);
