92747bac888cd689473d0c7663aeaa94a20f944d
GenesisCode-com <102680020+GenesisCode-com@users.noreply.github.com>
2024-05-01 15:18:11 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-// Middleware để thiết lập các tiêu đề HTTP
-app.use((req, res, next) => {
-  // Thiết lập tiêu đề Cache-Control
-  res.set('Cache-Control', 'public, max-age=3600'); // Cache trong 1 giờ
+// var LoremIpsum = require('lorem-ipsum').LoremIpsum;
 
-  // Thiết lập tiêu đề ETag
-  res.set('ETag', '123456'); // Thay '123456' bằng giá trị ETag thích hợp
-
-  // Thiết lập tiêu đề Last-Modified
-  res.set('Last-Modified', new Date().toUTCString());
-
-  next();
-});
+// var lorem = new LoremIpsum({
+//   sentencesPerParagraph: {
+//     max: 8,
+//     min: 4
+//   },
+//   wordsPerSentence: {
+//     max: 16,
+//     min: 4
+//   }
+// });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.get('/trk', (req, res) => {
-  // Chuyển hướng người dùng sang trang web của Google
-  res.redirect(301, 'https://www.google.com');
+    // Chuyển hướng sang trang web của Google
+    res.redirect(301, 'https://www.google.com');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
