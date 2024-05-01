76cfd3802ee85f4f1cb628f9ff3c525dcfde9fca
GenesisCode-com <102680020+GenesisCode-com@users.noreply.github.com>
2024-05-01 14:19:48 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const http = require('http');
 
-// var LoremIpsum = require('lorem-ipsum').LoremIpsum;
-
-// var lorem = new LoremIpsum({
-//   sentencesPerParagraph: {
-//     max: 8,
-//     min: 4
-//   },
-//   wordsPerSentence: {
-//     max: 16,
-//     min: 4
-//   }
-// });
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/trk', (req, res) => {
-  // Chuyển hướng sang trang web của Google
-  res.redirect(301, 'https://www.google.com');
+const server = http.createServer((req, res) => {
+  // Xử lý yêu cầu từ trình duyệt
+  if (req.url === '/trk') {
+    // Chuyển hướng người dùng sang trang web của Google
+    res.writeHead(302, { 'Location': 'https://www.google.com' });
+    res.end();
+  } else {
+    // Trả về trang không tìm thấy nếu đường dẫn không phù hợp
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 
