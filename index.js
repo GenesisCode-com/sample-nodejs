5eb2e72372386fefdda5a078a2b477825541640c
GenesisCode-com <102680020+GenesisCode-com@users.noreply.github.com>
2024-05-01 14:36:01 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 const port = process.env.PORT || 3000
 
 // var LoremIpsum = require('lorem-ipsum').LoremIpsum;
@@ -15,14 +15,36 @@ const port = process.env.PORT || 3000
 //   }
 // });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.get('/trk', (req, res) => {
-    // Chuyển hướng sang trang web của Google
-    res.redirect(301, 'https://www.google.com');
+// app.get('/trk', (req, res) => {
+//     // Chuyển hướng sang trang web của Google
+//     res.redirect(301, 'https://www.google.com');
+// });
+
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Xử lý yêu cầu từ trình duyệt
+  if (req.url === '/trk') {
+    // Chuyển hướng người dùng sang trang web của Google
+    res.writeHead(302, { 'Location': 'https://www.google.com' });
+    res.end();
+  } else {
+    // Trả về trang không tìm thấy nếu đường dẫn không phù hợp
+    res.writeHead(404);
+    res.end();
+  }
 });
 
+// Khởi động server trên cổng 8080
+server.listen(port, () => {
+  console.log('Server is running on port 8080');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
