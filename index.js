62226ec7fa0e8abd4757aa8af5ef273af0f4e0f4
GenesisCode-com <102680020+GenesisCode-com@users.noreply.github.com>
2024-05-01 12:14:10 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const express = require('express')
-// const app = express()
-// const port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
 // var LoremIpsum = require('lorem-ipsum').LoremIpsum;
 
@@ -15,7 +15,8 @@
 //   }
 // });
 
-// app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
